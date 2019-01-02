7d7428d421c13de65690a35985158bfb607ffca4
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-01-02 17:25:48 +0700

    Resolving context issues

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,18 +2,25 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+  // constructor(props) {
+  //   super(props);
+  //   this.onFormSubmit = this.onFormSubmit.bind(this);
+  // }
+
+
   state = {
     term: ''
   }
 
   onFormSubmit(event) {
     event.preventDefault(); //prevent browser's default action when submitting a form by pressing enter
+    console.log(this.state.term);
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
+        <form className="ui form" onSubmit={event => this.onFormSubmit(event)}>
           <div className="field">
             <label>Image Search</label>
             <input
