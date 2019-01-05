0cffc9e2c09a980fb81ddbe8e7a8745284770353
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-01-05 08:46:32 +0700

    setting images state in App after making a request

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,11 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-  async onSearchSubmit(term) {
+  state = {
+    images: [],
+  }
+
+  onSearchSubmit = async (term) => {
     // axioxs.get() return a promise obj
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
@@ -15,13 +19,14 @@ class App extends React.Component {
         Authorization: 'Client-ID 69616fb3c4ff340a477d0a761f4cec94ce06f558089d16553b115e663455b847',
       }
     });
-    console.log(response.data.results); 
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
