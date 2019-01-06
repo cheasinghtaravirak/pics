13f7fed038b17e4cd90045f50533f66fde946bdf
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-01-06 09:08:49 +0700

    rendering a list of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const ImageList = (props) => {
-  console.log(props.images);
+const ImageList = props => {
+  //images is an array of img element.
+  const images = props.images.map(image => <img src={image.urls.regular}></img>);
 
-  return <div>ImageList</div>;
+  return <div>{images}</div>;
 }
 
 export default ImageList;
