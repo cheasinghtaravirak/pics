aa10f078b1d922cdcf29400abd3ec2529e80b8c2
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-01-08 08:01:00 +0700

    use React Ref to access html DOM

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,12 +2,29 @@ import React from 'react';
 
 //ImageCard is chosen to be a class based component b/c to easy organize code
 class ImageCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    //instance variable
+    this.imageRef = React.createRef();
+  }
+  //Lifecycle method
+  componentDidMount() {
+    //current is html element. Thus,
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  //method
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  }
+
   render() {
     const { urls, description } = this.props.image;
-
     return (
       <div>
         <img
+          ref={this.imageRef}
           src={urls.regular}
           alt={description}
         />
